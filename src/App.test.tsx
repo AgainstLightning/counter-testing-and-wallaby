6644a8c4c7f07256bb3d581ac2bf5e0c0781bcf6
AgainstLightning <againstlightning@gmail.com>
2021-11-30 01:45:41 -0500

    add Count text

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,20 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders the title', () => {
+test('it renders the title', () => {
   render(<App />);
   const text = screen.getByText("Awesome Counter");
   expect(text).toBeInTheDocument();
+});
+
+test('it sets count text to 0 when startValue prop is not provided', () => {
+  render(<App />);
+  const count = screen.getByTestId("count");
+  expect(count).toHaveTextContent("0")
+});
+
+test('it sets count text to startValue prop when provided', () => {
+  render(<App startingValue={4}/>);
+  const count = screen.getByTestId("count");
+  expect(count).toHaveTextContent("4");
 });
