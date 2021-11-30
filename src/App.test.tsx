35a831d50dbff4f8d15637a66cb2fb46bcd1650b
AgainstLightning <againstlightning@gmail.com>
2021-11-30 17:12:00 -0500

    remove original buttons from App

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -8,28 +8,14 @@ test('it renders the title', () => {
   expect(text).toBeInTheDocument();
 });
 
-test('it sets count text to 0 when startValue prop is not provided', () => {
+test('it sets count text to 0 when startingValue prop is not provided', () => {
   render(<App />);
   const count = screen.getByTestId("count");
   expect(count).toHaveTextContent("0")
 });
 
-test('it sets count text to startValue prop when provided', () => {
+test('it sets count text to startingValue prop when provided', () => {
   render(<App startingValue={4}/>);
   const count = screen.getByTestId("count");
   expect(count).toHaveTextContent("4");
-});
-
-test('it renders the add button', () => {
-  render(<App />);
-  const addBtn = screen.getByTestId("addBtn");
-  expect(addBtn).toBeInTheDocument();
-  expect(addBtn).toHaveTextContent("+");
-});
-
-test('it renders the subtract button', () => {
-  render(<App />);
-  const subtractBtn = screen.getByTestId("subtractBtn");
-  expect(subtractBtn).toBeInTheDocument();
-  expect(subtractBtn).toHaveTextContent("-");
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
+import CounterButton from './components/CounterButton'
 
 function App({startingValue = 0} : {startingValue?: number}) {
+
+  const clickHandler = (amount: number) => {}
+
   return (
     <div className="App">
       <h1>Awesome Counter</h1>
       <p data-testid="count">{startingValue}</p>
-      <button data-testid="addBtn">+</button>
-      <button data-testid="subtractBtn">-</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
